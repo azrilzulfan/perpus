809fe2eeb4eb2783439f84206eec9426f4b2e9a4
MuhammadArsyPutra <mhmmdarsyputra123@gmail.com>
2023-09-21 22:24:10 +0700

    first commit

---
+++ resources/views/view_home.blade.php
@@ -0,0 +1,29 @@
+@extends('index')
+@section('title', 'Home')
+
+@section('isihalaman')
+    <h3>Pengertian perpustakaan menurut para ahli</h3>
+    <p>
+    <h4>Perpustakaan</h4>
+        Perpustakaan Perguruan Tinggi merupakan unit kerja pelaksana teknis (UPT) Perguruan Tinggi yang bersama-sama 
+		dengan unit lain turut melaksanakan Tri Dharma Perguruan Tinggi, yaitu: pendidikan, penelitian, dan pengabdian 
+		kepada masyarakat dengan cara memilah, menghimpun, mengolah, merawat, serta menyebarluaskan sumber informasi kepada 
+		lembaga induknya pada khususnya dan sivitas akademika pada umumnya. Kelima tugas tersebut dilaksanakan dengan 
+		tata cara administrasi dan organisasi yang berlaku bagi penyelenggaraan sebuah perpustakaan.
+    <p>
+    <h4>Perpustakaan SMKN 4 BOGOR</h4>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque cum sequi quos alias quas ex omnis cumque 
+        cupiditate obcaecati voluptates? Enim expedita non quasi, 
+        sapiente tempora minima recusandae! Quos, earum! Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+        Culpa voluptatem expedita, odio labore ducimus eaque laborum! Nemo expedita aperiam obcaecati quos, 
+        amet natus quam quasi omnis id ex voluptatum ducimus.
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis officia sit iure tempore, 
+        assumenda rerum nostrum non iste exercitationem. Enim omnis autem sit, 
+        cupiditate nostrum itaque deserunt? Fugit, architecto reprehenderit!
+    <p>
+    <h4>Perpustakaan Menurut UU NO 43. THN. 2007</h4>
+    Perpustakaan adalah institusi yang mengumpulkan pengetahuan tercetak dan terekam, mengelolanya dengan cara khusus guna
+    memenuhi kebutuhan intelektualitas para penggunanya melalui berbagai cara interaksi pengetahuan.
+
+@endsection
